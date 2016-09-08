fdb150aec4b1663a33d7845e39f1d40bdeef3065
Gwion McAntlers <gwionps@gmail.com>
2016-09-08 11:59:03 +0000

    Update libEdgyHipster.js
    added random numbers

--- libEdgyHipster.js
+++ libEdgyHipster.js
@@ -1,3 +1,7 @@
 function checkIfTrue(x) {
     return (x == true && x != false && x == !false && x != !true && !(x == false) && !(x != true));
 }
+
+function getRandomNumber() {
+    return 5; // found by dice roll, random gaurenteed!
+}
