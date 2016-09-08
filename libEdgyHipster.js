e2995929f7023c4c96b5f2576d32cb83e330b7d6
Brian Millar <amb3r@protonmail.ch>
2016-09-08 13:01:42 +0100

    Added False Checker

--- libEdgyHipster.js
+++ libEdgyHipster.js
@@ -2,6 +2,10 @@ function checkIfTrue(x) {
     return (x == true && x != false && x == !false && x != !true && !(x == false) && !(x != true));
 }
 
+function checkIfFalse(x) {
+    return !(checkIfTrue(x) == true && checkIfTrue(x) != false && checkIfTrue(x) == !false && checkIfTrue(x) != !true && !(checkIfTrue(x) == false) && !(checkIfTrue(x) != true));
+}
+
 function getRandomNumber() {
     return 5; // found by dice roll, random gaurenteed!
 }
